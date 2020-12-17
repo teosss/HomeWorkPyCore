eed4383be4513d38e74935b2c9a7236be74b80f1
Віктор Репський <forsmileledy@gmail.com>
2020-12-17 13:32:18 +0200

    update task 2

--- lesson_8/task_2.py
+++ lesson_8/task_2.py
@@ -2,7 +2,7 @@ import re
 
 def pass_validation():
     password = input("Please enter the password: ")
-    if len(password) in range(6,16) and re.search(r'[0-9]', password) and re.search(r'[a-z]', password) and re.search(r'[A-Z]', password) and re.search(r'[$#@]', password):
+    if len(password) in range(6,16) and re.search(r'[0-9]', password) and re.search(r'[a-z]', password) and re.search(r'[A-Z]', password) and re.search(r'[$#@]', password) and not re.search(" ", password):
         return "Password  is valid"
     else: 
         print("Password is not valid, try again")
