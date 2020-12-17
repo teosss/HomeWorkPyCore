c21efcd46c572afd74aeec5a381154e60d437342
Віктор Репський <forsmileledy@gmail.com>
2020-12-17 13:35:42 +0200

    update task 1

--- lesson_8/task_1.py
+++ lesson_8/task_1.py
@@ -16,10 +16,10 @@ def input_area(value):
         return super_module_t1.rectangle_area(s,b)
     elif value is '2':
         s = int(input("Enter the base of the triangle: "))
-        h = int(input("Enter the height of the triangle "))
+        h = int(input("Enter the height of the triangle: "))
         return super_module_t1.triangle_area(s,h)
     elif value is '3': 
-        r = int(input("Enter the height of the triangle "))
+        r = int(input("Enter radius of circle: "))
         return super_module_t1.circle_area(r)
     else:
         value = input("You have entered incorrect data, please try again (1,2,3): ")
