259c8079b1b96f388f34b02c26542cca03c28a00
Віктор Репський <forsmileledy@gmail.com>
2020-12-22 19:40:20 +0200

    update

--- lesson_11/task_2.py
+++ lesson_11/task_2.py
@@ -2,8 +2,8 @@ def day_of_week(number):
     weekDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
     try:
         number = int(number)
-        if number > 7:
-            raise ValueError("Your number > 7")     
+        if number > 7 or number <= 0:
+            raise ValueError()     
         return weekDays[number-1]             
     except ValueError as e:
         print("Please enter correct number. ", e)
